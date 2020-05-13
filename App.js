eeb9345dd2380e3b5c74d1058f69c1d2b4422f3f
liumin53 <gavin.liu@mail.utoronto.ca>
2020-05-13 12:35:01 -0400

    Added firebase Compatibility

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import * as firebase from "firebase";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC6PmdF76X3Mbp21ol8pGEiZCO8A8W_sQQ",
+  authDomain: "chatroom-13955.firebaseapp.com",
+  databaseURL: "https://chatroom-13955.firebaseio.com",
+  projectId: "chatroom-13955",
+  storageBucket: "chatroom-13955.appspot.com",
+};
+
+// Make sure Firebase is only called once when we refresh.
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
@@ -12,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
